854c7bb24d21475ff12c799589f23a4fc37ec5ff
Jean-Louis Leysens <jloleysens@gmail.com>
2022-02-15 11:04:19 +0100

    get the worker thread error in memory

--- src/worker.ts
+++ src/worker.ts
@@ -1,45 +1,24 @@
-import { parentPort, isMainThread, resourceLimits } from 'worker_threads';
-import { WorkerRequest, WorkerResponse } from '.';
-
-if (!isMainThread) {
-  parentPort!.on('message', innerWorker);
-}
+import { resourceLimits } from 'worker_threads';
 
 // Give Node.js a chance to move the memory to the old generation region
 const WAIT = 40;
 
-function innerWorker({ data, port }: WorkerRequest): WorkerResponse {
-  const allocateMemory = async () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(
-          new Array(resourceLimits.maxYoungGenerationSizeMb! * 1024 * 1024)
-            .fill('')
-            .map((_, idx) => idx) // more unique values prevent aggressive memory compression and hits mem limits faster
-        );
-      }, WAIT);
-    });
-  };
-
-//   (async function run() {
-//     const memoryLeak = [];
-//     for (;;) /* a computer crying */ {
-// 
-//       memoryLeak.push(await allocateMemory());
-//     }
-//   })();
-
-  const randomError = Math.random() * 10 >= 9 ? 'RANDOM ERROR' : undefined;
-
-  if (randomError) {
-    throw new Error(randomError);
-  }
-
-  const workerResponse: WorkerResponse = {
-    data: `Your message back from the worker: ${data}`,
-    error: randomError,
-  };
-
-  port.postMessage(workerResponse);
-  return workerResponse;
+const allocateMemory = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(
+        new Array(resourceLimits.maxOldGenerationSizeMb! * 1024)
+          .fill('')
+          .map((_, idx) => idx) // more unique values prevent aggressive memory compression and hits mem limits faster
+      );
+    }, WAIT);
+  });
 }
+
+(async function run() {
+  const memoryLeak = [];
+  for (;;) /* a computer crying */ {
+
+    memoryLeak.push(await allocateMemory());
+  }
+})();
